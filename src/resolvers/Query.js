3100d7f2a3e0f5addba237564d22892215fdc2c7
James Gan <teowgan@gmail.com>
2018-04-26 13:52:28 +1000

    Initial commit

---
+++ src/resolvers/Query.js
@@ -0,0 +1,22 @@
+// async function feed(parent, args, ctx, info) {
+  function feed(parent, args, ctx, info) {
+    const { filter, first, skip } = args; // destructure input arguments
+    const where = filter
+      ? { OR: [{ url_contains: filter }, { description_contains: filter }] }
+      : {};
+  
+    // const allLinks = await ctx.db.query.links({})
+   //  const count = allLinks.length
+  
+    // const queriedLinkes = await ctx.db.query.links({ first, skip, where })
+    return ctx.db.query.links({ first, skip, where }, info);
+
+    // return {
+    //   linkIds: queriedLinkes.map(link => link.id),
+    //   count
+    // }
+  }
+  
+  module.exports = {
+    feed,
+  }
